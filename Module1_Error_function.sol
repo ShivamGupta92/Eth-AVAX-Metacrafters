4e49bf2186f20838bb23e1bee8ab22f36ba02c06
Shivam Gupta <70855458+ShivamGupta92@users.noreply.github.com>
2023-07-07 12:12:49 +0530

    Update Module1_Error_function.sol

--- Module1_Error_function.sol
+++ Module1_Error_function.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.7;
 
 contract NumberChecker {
     function requireStatement(uint256 number) public pure  {
+        
         require(number > 0, "Number must be greater than zero");
         // return "Executed Succefully";
     }
     
     function revertStatement(uint256 number) public pure returns (string memory){
-       
+    //    changes back to intital stage 
         if (number % 2 == 0) {
             return "NumberType => Even";
         } else {
@@ -16,7 +17,12 @@ contract NumberChecker {
         }
     }
     
-    function assertStatement(uint256 number) public pure {
+    function assertStatement(uint256 number) public pure returns (string memory){
         assert(number < 100 && number >= 0);
+        if (number != 55){
+            return "Better Luck next time";
+        }
+        return "You have won the lottery";
+        // To avoid conditions that should never be possible
     }
 }
